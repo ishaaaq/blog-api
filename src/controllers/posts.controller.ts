31d9028731d649b25afea6633a981f67a2864d6d
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-15 00:07:35 +0100

    implement post creation function

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
-import { posts } from "../models/data"
-
+import { posts, users } from "../models/data"
+import crypto from 'crypto'
+import { post } from "../types"
 export class Posts{
     public getAllPosts = async (req: Request, res: Response) => {
         return res.status(200).json({posts: posts})
@@ -26,7 +27,42 @@ export class Posts{
     res.status(500).json({message: "Internal server error"})
    }
     }
-    public createPost = async () => {
+
+    //check if the user is authenticated, check all post properties are present and then create the post
+    public createPost = async (req: Request, res: Response) => {
+        const {userId} = req.params
+        const {title, description} = req.body
+
+        const legitUser = users.find(user => user.id === userId)
+
+        try {
+        if(!userId || !legitUser){
+            return res.status(500).json({message: "You must be authenticated to create a post"})
+        }
+
+        if (!title || !description){
+            return res.status(500).json({message: "Post must have a title and a description"})
+        }
+
+        const newPost: post = {
+            id: crypto.randomUUID() as string,
+            title,
+            description,
+            authorId: userId as string,
+            createdAt: Date.now(),
+            updatedAt: 0
+        }
+
+        posts.push(newPost)
+
+        return res.status(201).json({
+            message: "Post successfully created",
+            data: newPost
+        })
+    } catch (error){
+         console.log("Error creating post", error)
+        res.status(500).json({message: "Internal server error"})
+    }
 
     }
     public updatePost = async () => {
