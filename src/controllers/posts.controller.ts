697f5535cd11b47b03b88ca57436142793689868
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-14 23:09:46 +0100

    create post array and implement its getter function

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,9 +1,30 @@
-export class Posts{
-    public getAllPosts = async () => {
-        
-    }
-    public getOnePost = async () => {
+import { Request, Response } from "express"
+import { posts } from "../models/data"
 
+export class Posts{
+    public getAllPosts = async (req: Request, res: Response) => {
+        return res.status(200).json({posts: posts})
+    }
+    public getOnePost = async (req: Request, res: Response) => {
+        //get the post id, search for the post, return the post
+        const {id} = req.params
+        if (!id){
+          return  res.status(500).json({message: "Invalid request. Please provide post id"})
+        }
+        try {
+        const thePost = posts.find(post => post.id === id)
+        if(!thePost){
+           return res.status(500).json({message: "Post doent exist"})
+        }
+ 
+        return res.status(200).json({
+            message: "post found",
+            data: thePost
+        })
+   } catch (error) {
+    console.log("Error finding post", error)
+    res.status(500).json({message: "Internal server error"})
+   }
     }
     public createPost = async () => {
 
--- src/models/data.ts
+++ src/models/data.ts
@@ -1,3 +1,11 @@
-import { user } from "../types";
+import { post, user } from "../types";
 
-export let users: user[] = []
+export let users: user[] = [{
+        id: "1c3556e9-f6a8-486f-9598-5ee829dd1132",
+        email: "ishaqabdullahi660@gmail.com",
+        name: "ishaq Abdullahi",
+        passwordHash: "$2b$05$ng48QUT4xdpR0uWHNf6Wz.pKTPfVF..8H4/CO/uwOhW8FRxKbB18O",
+        createdAt: 1776203435859
+    }]
+
+export let posts: post[] = []
