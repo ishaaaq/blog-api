7cb184c8dc124f57f7af3561dc79683b602a8d4f
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-14 22:53:58 +0100

    fix routing bug in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import router from './routes/users.route';
 import postsRouter from './routes/posts.route';
 import usersRouter from './routes/users.route';
 import authRoute from './routes/auth.route';
@@ -7,9 +6,12 @@ import authRoute from './routes/auth.route';
 const app = express()
 
 app.use(express.json()) //parse incoming json bodies
-app.use(router)
 app.use('/users', usersRouter)
 app.use('/posts', postsRouter)
 app.use('/auth', authRoute)
+app.use('/', (req, res) => {
+    console.log("request recieved")
+    res.send("Hello from local host")
+})
 
 export default app;
