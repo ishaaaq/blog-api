10aedfff1b6ce88a0c70af265cf82f44fd110c7b
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-14 22:53:39 +0100

    implement getAllUsers function

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,10 +1,14 @@
+import { users } from "../models/data"
+import { Request, Response } from "express"
 export class Users {
      public getOneUser= async () => {
         
     }
 
-     public getAllUsers= async () => {
-        
+     public getAllUsers= async (req: Request, res: Response) => {
+        res.status(200).json({
+            data: users
+        })
     }
 
      public createUser= async () => {
