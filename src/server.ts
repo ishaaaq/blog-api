6357e2ea1005e1bd3159029fd174a86afe54835e
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-16 17:53:20 +0100

    add  log in entry file to show loaded env variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app'
 import 'dotenv/config'
+
+console.log("check .env varaiblaes:", process.env.JWT_SECRET)
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {console.log(`server listening on port ${PORT}`)})
