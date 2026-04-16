be9b9af6cd48926647ad3d7e762daa98ea1c162a
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-16 18:59:27 +0100

    implement update comment function

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { commentsArr } from "../models/data"
+import { commentsArr, posts } from "../models/data"
 import crypto from 'crypto'
 import { comment } from "../types"
 export class comments{
@@ -54,8 +54,8 @@ export class comments{
             }
 
             commentsArr.push(newComment)
-
-            return res.status(200).json({message: "success", comment: newComment})
+            console.log(`newly created comment ${newComment.postId}`)
+            return res.status(200).json({message: "success", comment: newComment, postID: `im here ${postId}`})
 
         } else {
             return res.status(401).json({message: "Please login to comment"})
@@ -64,8 +64,46 @@ export class comments{
             return res.status(500).json({message: "Internal server error"})
         }
     }
-    public updateComment = async () => {
 
+    // /posts/postId/comments/commentId
+    //check the user is authenticated, check he's authorrized to edit, check both post and comment exist, save new value
+    public updateComment = async (req: Request, res: Response) => {
+        console.log("recieved in updatecomment:")
+        try {
+            if(req.user){
+                
+                const { postId, commentId } = req.params
+                const { body } = req.body
+
+                const post = posts.find(p => p.id === postId)
+                const comment = commentsArr.find(c => c.id === commentId)
+
+                if (!post || !comment){
+                    return res.status(400).json({message: "Comment or post dosent exist"})
+                }
+
+                if(comment.authorId !== req.user.id){
+                    return res.status(403).json({message: "You are unauthorized to edit this comment!"})
+                }
+
+                const newComment = {
+                    ...comment,
+                    body: body
+                }
+
+                const commentIndex = commentsArr.indexOf(comment)
+
+                commentsArr.splice(commentIndex, 1, newComment)
+                console.log("new comments array, check updated", commentsArr)
+
+                return res.status(200).json({message: "success", updatedComment: newComment})
+            } else {
+                return res.status(401).json({message: "Please login to edit comment"})
+            }
+        } catch (error) {
+            console.log("Error updating comment:", error)
+            return res.status(500).json("Internal server error")
+        }
     }
     public  deleteComment = async () => {
 
