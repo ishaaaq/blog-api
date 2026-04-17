a74248c34adb8cd93f4bf9971cc107066d14fde9
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-17 23:19:46 +0100

    feat: implement update profile function

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -15,7 +15,41 @@ export class Users {
         
     }
 
-     public updateUser= async () => {
-        
+    //check user is authennticated, check authorization, check id exists
+     public updateUser= async (req: Request, res: Response) => {
+
+        try {
+        const { email, name } = req.body
+        if(!req.user){
+            return res.status(401).json({message: "please login to edit profile"})
+        }
+
+        if(req.user.id !== req.params.userId){
+            return res.status(403).json({message: "You are unauthorized to edit this profile"})
+        }
+
+        const user = users.find(u => u.id === req.params.userId)
+        if(!user){
+            return res.status(400).json({message: "User not found!"})
+        }
+
+       const newUser = {
+        ...user,
+        name,
+        email, 
+       }
+
+       const userIndex = users.indexOf(user)
+       users.splice(userIndex, 1, newUser)
+
+       return res.status(200).json({
+        message: "success",
+        updatedUser: newUser
+       })
+    
+      } catch (error) {
+            console.log("Error editing profile: ", error)
+            return res.status(500).json("Internal server error")
+        }
     }
 }
