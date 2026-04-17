563f9ccd318521933f2c8f5a6ab19d5f9ecf7d12
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-17 22:57:41 +0100

    add authmiddleware to update user route

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import postsRouter from './posts.route'
 import { Users } from '../controllers/users.controller'
+import { authMiddleware } from '../middlewares/auth.middleware'
 const usersRouter = express.Router()
 
 const usersController = new Users()
@@ -8,7 +9,7 @@ const usersController = new Users()
 usersRouter.get('/:userId', usersController.getOneUser)
 usersRouter.get('/', usersController.getAllUsers)
 usersRouter.post('/', usersController.createUser)
-usersRouter.put('/:userId', usersController.updateUser)
+usersRouter.put('/:userId', authMiddleware, usersController.updateUser)
 
 
 usersRouter.use('/:userId/posts', postsRouter)
