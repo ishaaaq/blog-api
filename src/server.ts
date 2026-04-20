bf4290b84a65d90866a3d5e166c9830b37e19acd
ishaq abdullahi <abdullahiishaqgarba@gmail.com>
2026-04-20 23:08:40 +0100

    remove console.log for jwt env variable in server file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import app from './app'
 import 'dotenv/config'
 
-console.log("check .env varaiblaes:", process.env.JWT_SECRET)
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {console.log(`server listening on port ${PORT}`)})
+app.listen(PORT, () => { console.log(`server listening on port ${PORT}`) })
